9e6de1454b5193b091e115ecd249f33f342e0c14
up201806330 <45789887+up201806330@users.noreply.github.com>
2021-04-12 02:21:20 +0100

    Scripts for delivery

---
+++ proj1/src/build/cleanup.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/bash
+
+# Placeholder for clean-up script
+# To be executed in the root of the build tree
+# Requires at most one argument: the peer id
+# Cleans the directory tree for storing 
+#  both the chunks and the restored files of
+#  either a single peer, in which case you may or not use the argument
+#    or for all peers, in which case you 
+
+
+# Check number input arguments
+argc=$#
+
+if ((argc > 1 ))
+then
+	echo "Usage: $0 [<peer_id>]]  | $0"
+	exit 1
+elif ((argc < 0))
+then
+	echo "Usage: $0 [<peer_id>]] | $0"
+	exit 1
+elif ((argc == 0))
+then
+  for i in {1..10}
+  do
+    rm -rf service-$i
+  done
+else
+	peer_id=$1
+  rm -rf service-${peer_id}
+fi
+
+
